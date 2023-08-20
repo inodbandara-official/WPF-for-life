776f5807ac4e04e4d0258b7b6efa92bab254134b
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-08-20 11:49:50 +0530

    Made Details.cs
    Just the beginning...

---
+++ Details.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace AwesomeDotNetCore
+{
+    internal class Details
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("This is C# repository mixed with some other languages.");
+
+            Console.WriteLine("Made this repository for C# users.");
+
+            Console.WriteLine("Your welcome in Advance.");
+        }
+    }
+}
